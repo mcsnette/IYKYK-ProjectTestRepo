321603020ce4c1e2820626f78d934876d7840a97
John Villamor <Villamor.john182000@gmail.com>
2023-12-05 01:02:47 +0800

    Login/Register Completed
    Login and register have been integrated by using firebase authentication

--- app/src/main/java/com/example/iykyk_project/MainActivity.kt
+++ app/src/main/java/com/example/iykyk_project/MainActivity.kt
@@ -5,31 +5,97 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.example.iykyk_project.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.reference.child("Users")
+
+
+
+
+        //Register button Listener
         val RegisterBtn = findViewById<Button>(R.id.RegBtn)
-
         RegisterBtn.setOnClickListener {
             val intent = Intent(this@MainActivity, Register_Activity::class.java)
             startActivity(intent)
+            finish()
         }
 
+        //forgot button listener
         val forgotPwTxt: TextView = findViewById(R.id.ForgotPwTxt)
-
         forgotPwTxt.setOnClickListener {
             val intent = Intent(this@MainActivity, ForgetPassword_Activity::class.java)
             startActivity(intent)
         }
 
-        val LoginBtn = findViewById<Button>(R.id.loginBtn)
+        binding.loginBtn.setOnClickListener(){
+            //variables from Register Page
+            val LoginUsername = binding.UsernameTxt.text.toString()
+            var LoginPassword= binding.PasswordTxt.text.toString()
 
-        LoginBtn.setOnClickListener {
-            val intent = Intent(this@MainActivity, Homepage_Activity::class.java)
-            startActivity(intent)
+
+            if (LoginUsername.isNotEmpty() && LoginPassword.isNotEmpty()) {
+                //LoginUser(LoginUsername,LoginPassword)
+                firebaseAuth.signInWithEmailAndPassword(LoginUsername, LoginPassword).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        val intent = Intent(this, Homepage_Activity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+            }
+
+
+
         }
+            else {Toast.makeText(this@MainActivity, "fill up the damn text fields! nagdabog",Toast.LENGTH_SHORT).show()
+            }
+    }
+
+
+    /*private fun LoginUser (Username: String, Password:String){
+        databaseReference.orderByChild("Username").equalTo(Username).addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onDataChange(datasnapshot: DataSnapshot) {
+                if (datasnapshot.exists()){
+                    for (userSnapshot in datasnapshot.children){
+                        val userData = userSnapshot.getValue(UserData::class.java)
+
+                        if(userData != null && userData.Password != null){
+                            Toast.makeText(this@MainActivity,"Login Success! hooray!", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this@MainActivity, Homepage_Activity::class.java))
+                            finish()
+                            return
+                        }
+                    }
+                }
+                Toast.makeText(this@MainActivity,"Your Credentials are Incorrect", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity, "Database Error: ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }*/
     }
 }
---
+++ app/src/main/java/com/example/iykyk_project/UserData.kt
@@ -0,0 +1,8 @@
+package com.example.iykyk_project
+
+data class UserData(
+    val id: String? = null,
+    val Email: String? = null,
+    val Username: String? = null,
+    val Password: String? = null,
+)
