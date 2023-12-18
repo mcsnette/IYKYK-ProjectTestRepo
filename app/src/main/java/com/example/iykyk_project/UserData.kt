a0a959df5de933be604e469bfe7a711dfae5c614
John Villamor <Villamor.john182000@gmail.com>
2023-12-18 16:31:41 +0800

    Edit Profile is functional

--- app/src/main/java/com/example/iykyk_project/UserData.kt
+++ app/src/main/java/com/example/iykyk_project/UserData.kt
@@ -4,7 +4,8 @@ data class UserData(
     var id: String? = null,
     var email: String? = null,
     var username: String? = null,
-    var password: String? = null
+    var password: String? = null,
+    var fullname: String? = ""
 ) {
     constructor(username: String?) : this() {
         this.username = username
